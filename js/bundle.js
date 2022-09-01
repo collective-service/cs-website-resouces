87d6fa916cac1e75061ffe6e5b6af5746da66991
amadu <ndongamadou@gmail.com>
2022-09-01 16:39:59 +0100

    layout and data

--- js/bundle.js
+++ js/bundle.js
@@ -2,28 +2,64 @@
 //"https://docs.google.com/spreadsheets/d/e/2PACX-1vRpEYi7ap7o89ZILw58orkWksC2MlsOWS9Q4loIXgb46qfKxRNp01QmY-FGwHLi5NZNz80Aa_k3Pjt5/pub?gid=1612863274&single=true&output=csv";
 const dataURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZmYvJRU25HAiWMu6xscYINvLxMWmTNls474hTIczFrnMI_afgtaXl5PPUwiSIfU0L6Zjfhn9wtv03/pub?gid=0&single=true&output=csv";
 const configURL = "data/config.json";
+const dictURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZmYvJRU25HAiWMu6xscYINvLxMWmTNls474hTIczFrnMI_afgtaXl5PPUwiSIfU0L6Zjfhn9wtv03/pub?gid=238404823&single=true&output=csv";
 
 let config;
 let resourcesData,
-    filteredResourcesData;
+    filteredResourcesData,
+    dictionary;
 
 let dataTable;
+let advancedSearchItems = [];
+
 
 $(document).ready(function() {
     function getData() {
         Promise.all([
             d3.json(configURL),
-            d3.csv(dataURL)
+            d3.csv(dataURL),
+            d3.csv(dictURL)
         ]).then(function(data) {
             config = data[0];
             resourcesData = data[1];
             filteredResourcesData = resourcesData;
 
+            dictionary = data[2];
+
+            // set in config Dropdowns the data from dictionary
+            for (k in config.Dropdowns) {
+                const drop = config.Dropdowns[k].name;
+                for (let index = 0; index < dictionary.length; index++) {
+                    const tupl = dictionary[index];
+                    if (tupl.key == drop) {
+                        config.Dropdowns[k].data = formatListToArray(tupl.value);
+                        break;
+                    }
+                }
+            }
+
+            // for (k in config.Dropdowns) {
+            //     console.log(config.Dropdowns[k])
+            // }
             generateTags();
 
             createHtmlTable();
 
+            // create advanced search items (select and tags)
+            for (k in config.Dropdowns) {
+                const id = config.Dropdowns[k].id;
+                if (id != "null") {
+                    generateAdvancedSearchItem(k);
+                }
+            }
+
+            var tagButtons = document.getElementsByClassName("advancedTag");
+            for (var i = 0; i < tagButtons.length; i++) {
+                tagButtons[i].addEventListener('click', addTagtoFilter);
+            }
+
             generateDataTable();
+            dataTable.columns.adjust().draw();
 
         }); // then
     } // getData
@@ -31,6 +67,53 @@ $(document).ready(function() {
     getData();
 
 });
+
+function generateAdvancedSearchItem(name) {
+    var type = "",
+        id = "",
+        data;
+    var html;
+
+    for (k in config.Dropdowns) {
+        if (String(config.Dropdowns[k].name) === String(name)) {
+            type = config.Dropdowns[k].type;
+            data = config.Dropdowns[k].data;
+            id = config.Dropdowns[k].id;
+            break;
+        }
+    }
+    const classeName = name.toLowerCase();
+    if (type == "tag") {
+        // create tag filter
+        var tags = "";
+        for (let index = 0; index < data.length; index++) {
+            const element = data[index];
+            tags += '<label><button type="button" class="btn advancedTag" id="' + element + '" value="' + element + '">' + element + '</button></label>';
+        }
+        html = tags;
+    } else {
+        // create dropdwown select
+        var options = "";
+        data.unshift('Select all'); // finalement done in the
+        for (let i = 0; i < data.length; i++) {
+            const element = data[i];
+            i == 0 ? options += '<option value="all" selected>' + element + '</option>' :
+                options += '<option value="' + element + '">' + element + '</option>';
+        }
+        html = options;
+    }
+    $('#' + id).append(html);
+} //generateAdvancedSearchItem
+
+function getSelectedTags(elementID) {
+    var items = $("#" + elementID + " label button");
+    var selections = [];
+    items.each(function(idx, btn) {
+        const isSelected = $(btn).hasClass('is-selected');
+        isSelected ? selections.push($(btn).text()) : null;
+    });
+    return selections;
+} //getSelectedTags
 
 function generateTags() {
     var tagsToDisplay = '<label><strong>Filter by: </strong></label><label><button type="button" class="btn tagFilter tag-all active" id="all" value="all">All</button></label>';
@@ -52,25 +135,6 @@ function generateTags() {
 
 } //generateTags
 
-function clickButton() {
-    $('.btn').removeClass('active');
-    var tagSelected = this.value;
-    var data = resourcesData;
-
-    if (tagSelected == "all") {
-        updateDataTable(data);
-
-    } else {
-        var filteredData = data.filter(function(d) {
-            var tagArr = getFormattedDimension(d[config.tagsColName]);
-            return tagArr.includes(tagSelected) ? d : null;
-        });
-        updateDataTable(filteredData);
-    }
-
-    $(this).toggleClass('active');
-
-} //clickButton
 
 // Creer a partir de la liste de config.TableHeadersCleaned les entetes de la table
 function createHtmlTable() {
@@ -92,16 +156,22 @@ function slugify(texte) {
         .replace(/ +/g, '-');
 }
 
-// get dimensions formatted in tags
-function getFormattedDimension(item) {
+function formatListToArray(liste) {
     var items = [];
-    var arr = item.split(",");
+    var arr = liste.split(",");
     var trimedArr = arr.map(x => x.trim());
     for (let index = 0; index < trimedArr.length; index++) { //remove empty elements
         if (trimedArr[index]) {
             items.push(trimedArr[index]);
         }
     }
+    return items;
+} //formatListToArray 
+
+// get dimensions formatted in tags using sep = comma
+function getFormattedDimension(item) {
+    var items = formatListToArray(item);
+
     var formatedDims = "";
     items.forEach(element => {
         var className = slugify(element);
@@ -113,54 +183,39 @@ function getFormattedDimension(item) {
 // generer une array de donnees basee sur la structure de la table
 function getDataTableData(data = filteredResourcesData) {
     var dt = [];
-    var headers = config.TableHeaders;
+    var headers = config.OthersTableColumns;
     data.forEach(element => {
-        var vals = []; //start with the id that is hidden, so i first push "" as first element of the array
-        vals.push(element[config.id]);
+        var document = "";
         for (let index = 0; index < headers.length; index++) {
             const hd = headers[index];
             var val = element[hd];
-            if (hd == config.Link) {
-                val = '<a href="' + element[hd] + '" target="blank"><i class="fa fa-external-link"></i></a>';
-            }
-            if (hd == config.tagsColName) {
-                val = getFormattedDimension(element[hd]);
-            }
-            vals.push(val)
+            hd == "Title" ? document += '<h5>' + val + '</h5>' :
+                hd == "Summary" ? document += '<h6>' + val + '</h6>' : document += '<p>' + val + '</p>';
         }
-        dt.push(vals)
+        dt.push([element[config.id], document])
     });
     return dt;
 } //getDataTableData
 
 function generateDataTable() {
     var dtData = getDataTableData();
-    console.log(dtData)
     dataTable = $('#datatable').DataTable({
         data: dtData,
-        "columns": [{ //// taille de la premiere col, source_id qui est hidden
-                "className": 'details-control',
-                "orderable": false,
-                "data": null,
-                "defaultContent": '<i class="fa fa-caret-down"></i>',
-                "width": "1%"
-            },
-            { "width": "15%" },
-            { "width": "25%" },
-            { "width": "10%" },
-            { "width": "5%" },
-            { "width": "5%" },
-            { "width": "5%" }
+        bAutoWidth: false,
+        aoColumns: [
+            { sWidth: null },
+            { sWidth: '100%', target: 1 }
         ],
+
         "columnDefs": [{
                 "className": "dt-head-left",
                 "targets": "_all"
             },
             {
-                "defaultContent": "-",
+                "defaultContent": " No document found",
                 "targets": "_all"
             },
-            // { "targets": [0], "visible": false } // si tu veux creer des colones caches, c'est pour l'export car seules les vals apparentes sont exportees
+            { "targets": [0], "visible": false }
             // { "targets": [8], "visible": false }, { "targets": [9], "visible": false }, { "targets": [10], "visible": false },
             // { "searchable" : true, "targets": "_all"},
             // {"type": "myDate","targets": 4}
@@ -170,113 +225,13 @@ function generateDataTable() {
         "paging": false,
         // "bLengthChange": false,
         // "pagingType": "simple_numbers",
-        "order": [
-            [1, 'asc'] // specifie ici le num de colone a sort par defaut
-        ],
+        "order": false,
         "dom": "Blrt"
     });
 
-    $('#datatable tbody').on('click', 'td.details-control', function() {
-        var tr = $(this).closest('tr');
-        var row = dataTable.row(tr);
-        if (row.child.isShown()) {
-            row.child.hide();
-            tr.removeClass('shown');
-            tr.css('background-color', '#fff');
-            tr.find('td.details-control i').removeClass('fa fa-caret-right');
-            tr.find('td.details-control i').addClass('fa fa-caret-down');
-        } else {
-            row.child(format(row.data())).show();
-            tr.addClass('shown');
-            tr.css('background-color', '#f5f5f5');
-            $('#cfmDetails').parent('td').css('border-top', 0);
-            $('#cfmDetails').parent('td').css('padding', 0);
-            $('#cfmDetails').parent('td').css('background-color', '#f5f5f5');
-            tr.find('td.details-control i').removeClass('fa-solid fa-caret-down');
-            tr.find('td.details-control i').addClass('fa-solid fa-caret-right');
-
-        }
-    });
-
+    dataTable.columns.adjust().draw();
 } //generateDataTable
 
-function format(arr) {
-    filtered = filteredResourcesData.filter(function(d) { return d['source_id'] == arr[0]; });
-    return '<table class="tabDetail" id="cfmDetails" >' +
-        '<tr>' +
-        '<td>&nbsp;</td>' +
-        '<td>&nbsp;</td>' +
-        '<td>&nbsp;</td>' + '<td>&nbsp;</td>' +
-        '<td>' +
-        '<table class="tabDetail" >' +
-        '<tr>' +
-        '<th><strong>Geo</strong></th>' +
-        '<td>Region</td>' +
-        '<td>' + filtered[0]['region'] + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>&nbsp;</td>' +
-        '<td>Countries (' + filtered[0]['country_count'] + ')</td>' +
-        '<td>' + filtered[0]['countries'] + '</td>' +
-        '</tr>' +
-
-        '<tr>' +
-        '<th><strong>Purpose</strong></th>' +
-        '<td>Summary</td>' +
-        '<td>' + filtered[0]['details'] + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>&nbsp;</td>' +
-        '<td>Indicators</td>' +
-        '<td>' + filtered[0]['variables'] + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>&nbsp;</td>' +
-        '<td>Target</td>' +
-        '<td>' + filtered[0]['target_pop'] + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<th><strong>Method</strong></th>' +
-        '<td>Survey</td>' +
-        '<td>' + filtered[0]['methodology'] + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>&nbsp;</td>' +
-        '<td>Sample</td>' +
-        '<td>' + filtered[0]['sample_type'] + ' - ' + filtered[0]['sample_size'] + ' respondents</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>&nbsp;</td>' +
-        '<td>Review</td>' +
-        '<td>' + filtered[0]['quality_check'] + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>&nbsp;</td>' +
-        '<td>Comment</td>' +
-        '<td>' + filtered[0]['source_comment'] + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        // '<th rowspan="3"><strong>Source</strong></th>'+
-        '<th><strong>Source</strong></th>' +
-        '<td>Data Type</td>' +
-        '<td>' + filtered[0]['access_type'] + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>&nbsp;</td>' +
-        '<td>Authors</td>' +
-        '<td>' + filtered[0]['authors'] + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>&nbsp;</td>' +
-        '<td>Publication</td>' +
-        '<td>' + filtered[0]['publication_channel'] + '</td>' +
-        '</tr>' +
-        '</table>' +
-        '</td>' +
-        '<td>&nbsp;</td>' +
-        '</tr>' +
-        '</table>'
-} //format
 
 function updateDataTable(data = filteredResourcesData) {
     var dt = getDataTableData(data);
@@ -297,4 +252,83 @@ $("#more-filters").on("click", function() {
     } else {
         d3.select(".advanced-filters").classed("hidden", true)
     }
-})
+});
+
+
+function addTagtoFilter() {
+    $('.advancedTag').removeClass('active');
+    const isSelected = $(this).hasClass('is-selected');
+    if (!isSelected) {
+        $(this).addClass('is-selected');
+    } else {
+        $(this).removeClass('is-selected');
+    }
+} //addTagtoFilter
+
+
+$("#search-apply").on("click", function() {
+    updateData();
+    updateDataTable();
+
+});
+
+$("#reset").on("click", function() {
+    for (k in config.Dropdowns) {
+        const id = config.Dropdowns[k].id;
+        if (id != "null") {
+            const type = config.Dropdowns[k].type;
+            if (type === "tag") {
+                // tags
+                $("#" + id + " label button").each(function(index, btn) {
+                    $(btn).hasClass('is-selected') ? $(btn).removeClass('is-selected') : null;
+                });
+            } else {
+                // select
+                $('#' + id).val("all");
+            }
+        }
+    }
+    filteredResourcesData = resourcesData;
+    updateDataTable();
+});
+
+
+// test two arrays : test if a element from arr is included in itemArr
+function compareArrays(itemArr, arr) {
+    var included = false;
+    for (var i = 0; i < arr.length; i++) {
+        if (itemArr.includes(arr[i])) {
+            included = true;
+            break;
+        }
+    }
+    return included;
+}
+
+function updateData() {
+    var data = resourcesData;
+    for (k in config.Dropdowns) {
+        const id = config.Dropdowns[k].id;
+        // make sure the DOM element exists
+        if (id != null) {
+            const type = config.Dropdowns[k].type;
+            var arr = [];
+            if (type === "tag") {
+                // tags
+                const selectionTagArr = getSelectedTags(id);
+                if (selectionTagArr != []) {
+                    data = data.filter(d => { return compareArrays(formatListToArray(d[k]), selectionTagArr); });
+                }
+            } else if (type === "select") {
+                // select
+                const selection = $('#' + id).val();
+                selection != "all" ?
+                    data = data.filter(d => { return d[k] == selection; }) : null;
+            }
+            // filteredResourcesData = data;
+        }
+    }
+
+    filteredResourcesData = data;
+
+} //updateData
